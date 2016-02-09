39ec21156046303e080ef79077d62f2626b2d0b1
Répás János <jrepas@icloud.com>
2016-02-09 12:20:33 +0100

    CSS style
    fixing the menu and the whole page

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,26 +26,33 @@
 	</head>
 	<body <?php body_class(); ?>>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+	<!-- header -->
+	<header class="header clear" role="banner">
 
-			<!-- header -->
-			<header class="header clear" role="banner">
+			<!-- logo -->
+			<div class="logo">
+				<a href="<?php echo home_url(); ?>">
+					<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+				</a>
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
+				<h1>911 Pizzabár</h1>
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+				<div class="phone">
+					<i class="fa fa-phone fa-lg"></i>
+					<a href="tel:+36-20-911-555"> +36-20-911-5555</a>
+				</div>
+			</div>
+			<!-- /logo -->
 
-			</header>
-			<!-- /header -->
+			<!-- nav -->
+			<nav class="nav" role="navigation">
+				<?php html5blank_nav(); ?>
+			</nav>
+			<!-- /nav -->
+
+	</header>
+	<!-- /header -->
+
+	<!-- wrapper -->
+	<div class="wrapper">
